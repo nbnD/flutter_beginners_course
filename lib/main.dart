851110836c507c88cc5847c3e1656997d4a48fca
Nabin <ndhakal10@gmail.com>
2023-02-22 16:29:07 +0545

    Lesson 2 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
           title: const Text("My First App"),
           centerTitle: true,
           backgroundColor: Colors.red[600]),
-      body:  Center(
-        child: Text(
-          "Hi Flutter Junction",
-          style: TextStyle(
-            fontSize:20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color:Colors.grey[600],
-            fontFamily: 'Poppins'
-          ),
+      body: Row(
+        mainAxisAlignment:MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+        const Text("Hello Buddies"),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text("Click"),
         ),
-      ),
+        Container(
+          color: Colors.cyan,
+          padding: const EdgeInsets.all(30),
+          child: const Text("Container"),
+        )
+      ]),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Colors.red[600],
-        child:const Text("Click"),
+        child: const Text("Click"),
       ),
-    ),
-  ));
+    );
+  }
 }
